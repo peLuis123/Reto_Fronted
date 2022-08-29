8c65f4b41f61052524f8019011e7723a0ec60c0a
Pedro Luis Ramos Calla <pedrorc2018@gmail.com>
2022-08-28 19:40:51 -0500

    se agrego la funcionalidad de la camara

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import WebCamRecoder from './camera'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-
+  <WebCamRecoder/>
   </React.StrictMode>,
   document.getElementById('root')
 )
